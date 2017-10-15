0ac619d5455d5abba8db1590996991ebfebd7708
sid1057 <ivanov.dale@gmail.com>
2017-10-15 19:04:45 +0300

    Points are filtered by the offset between frames

--- obstacle_detector/tm/core.py
+++ obstacle_detector/tm/core.py
@@ -2,6 +2,31 @@ import cv2
 import numpy as np
 
 
+def find_matches(gray1, gray2, shift_distance):
+    orb = cv2.ORB_create()
+
+    gray1 = cv2.bilateralFilter(gray1, 9, 75, 75)
+    gray2 = cv2.bilateralFilter(gray2, 9, 75, 75)
+
+    gray1 - cv2.equalizeHist(gray1)
+    gray2 - cv2.equalizeHist(gray2)
+
+    kp1, des1 = orb.detectAndCompute(gray1, None)
+    kp2, des2 = orb.detectAndCompute(gray2, None)
+    if des1 is None or des2 is None:
+        return np.concatenate((gray1, gray2), axis=1)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING,  crossCheck=True)
+
+    matches = bf.match(des1, des2)
+
+    matches = filter(lambda x: x.distance > shift_distance, matches)
+    matches = sorted(matches, key = lambda x: x.distance)
+
+    gray3 = cv2.drawMatches(gray1, kp1, gray2, kp2, matches[:], None, flags=2)
+    return gray3
+
+
 def get_random_square(img, square):
     x, y, width = square
     x1, y1, x2, y2 = x, y, x + width, y + width
@@ -10,7 +35,7 @@ def get_random_square(img, square):
 
 def find_template(frames, coords):
     img = frames[-1].copy()
-    old_img = frames[0].copy()
+    old_img = frames[-3].copy()
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     old_img = cv2.cvtColor(old_img, cv2.COLOR_BGR2GRAY)
@@ -36,11 +61,7 @@ def find_template(frames, coords):
     img = cv2.rectangle(img, coords[:2], original_bottom_right, 255, 1)
     old_img = cv2.rectangle(old_img, top_left, bottom_right, 255, 2)
     
-    ravel = res.reshape((-1))
-    max_value = max(ravel)
-    min_value = min(ravel)
-    res = res * 255 / max_value / 4
-    res = np.uint8(res)
-    res = cv2.equalizeHist(res)
+    shift_distance = y1 - coords[1]
+    matches = find_matches(match, old_template, shift_distance)
 
-    return img, old_img, np.concatenate((match, old_template), axis=1), absdiff
+    return img, old_img, matches, absdiff
