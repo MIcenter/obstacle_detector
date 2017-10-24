a9b65cfa3ce1fe4c9756c48bf3723da4deec4212
sid1057 <ivanov.dale@gmail.com>
2017-10-24 05:23:38 +0300

    Test version of image stitching for slam and obstacle detection

--- obstacle_detector/slam.py
+++ obstacle_detector/slam.py
@@ -3,30 +3,39 @@ import numpy as np
 
 
 def get_rectangle_from_img(img, rectangle):
-    x, y, width, height = rectangle
-    x1, y1, x2, y2 = x, y, x + width, y + height
+    x1, y1, x2, y2 = rectangle
     return img[y1:y2, x1:x2].copy()
 
 
-def find_shift_value(img, old_img, coords=(0, 50, 300, 200)):
+def find_shift_value(img, old_img, coords):
     old_template = get_rectangle_from_img(old_img, coords)
 
     res = cv2.matchTemplate(img, old_template, cv2.TM_CCORR_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
     top_left = max_loc
+    print('top_left', top_left)
 
     t_height, t_width = old_template.shape[:2]
 
-    return top_left[1] + t_height - coords[1] - coords[3]
+    return top_left[0] - coords[0], top_left[1] - coords[1]
 
 
-def stich_two_images(slam_map, img1, img2, shift_y=None):
-    print(find_shift_value(img2, img1))
+def stich_two_images(
+        slam_map, img1, img2,
+        x_excess=20,
+        shift_x=None, shift_y=None):
+
     if shift_y is None:
-        shift_y = find_shift_value(img1, img2)
-        shift_y = min(shift_y, 0)
+        y_from = img2.shape[0] - 200
+        y_to = img2.shape[0] - 100
+        shift_x, shift_y = find_shift_value(
+            img1,
+            img2,
+            coords=(x_excess, y_from, -x_excess, y_to))
+        shift_y = min(shift_y, 0) // 8
 
     shifted_img2 = np.roll(img2, shift_y, axis=0)
-    shifted_img2 = shifted_img2[:-shift_y,:]
-    return np.concatenate((shifted_img2, slam_map))
+    shifted_img2 = np.roll(shifted_img2, shift_x, axis=1)
+    shifted_img2 = shifted_img2[:-shift_y,x_excess:-x_excess]
+    return np.concatenate((shifted_img2, slam_map)), shift_x, shift_y
--- slam_test.py
+++ slam_test.py
@@ -14,6 +14,11 @@ from obstacle_detector.perspective import find_center_point
 from obstacle_detector.perspective import inv_persp_new
 from obstacle_detector.perspective import regress_perspecive
 
+from obstacle_detector.utils.gabor_filter import gabor_filter
+
+
+transformed_img_width = 200
+
 
 def video_test(input_video_path=None, output_video_path=None):
     cx = 595
@@ -34,31 +39,53 @@ def video_test(input_video_path=None, output_video_path=None):
         original_frames.append(frame)
 
         img, pts1 = inv_persp_new(
-            frame, (cx, cy), (roi_width, roi_length), spline_dist, 100)
+            frame, (cx, cy), (roi_width, roi_length),
+            spline_dist, transformed_img_width)
         transformed_frames.append(img)
 
         ret, frame = cap.read()
 
     cy, cy = find_center_point(original_frames, (400, 100, 800, 719))
 
-    past_img = transformed_frames[-5].frame
+    past_img = transformed_frames[-5].frame[300:,20:-20]
+    coord_y = 1
     while(ret):
         ret, frame = cap.read()
 
         img, pts1 = inv_persp_new(
-            frame, (cx, cy), (roi_width, roi_length), spline_dist, 100)
+            frame, (cx, cy), (roi_width, roi_length),
+            spline_dist, transformed_img_width)
         #img_gray, old_img_gray, match, absdiff = tm.find_obstacles(transformed_frames, (0, 50, 200, 400), method='sparse')
 
         transformed_frames.popleft()
         transformed_frames.append(img)
+        # transformed_frames.append(gabor_filter(img))
 
-        cv2.imshow('current', transformed_frames[-1].frame)
-        cv2.imshow('past', transformed_frames[0].frame)
+        slam_height, slam_width = past_img.shape[:2]
+        y_to = slam_height - coord_y -200
+        y_from = slam_height - coord_y - 300
 
-        past_img = stich_two_images(
+        print('position', y_from, y_to)
+
+#        cv2.imshow('current', transformed_frames[-1].frame)
+        cv2.imshow(
+            'frame position',
+            transformed_frames[-1].frame[-300 : -200,:])
+        cv2.imshow(
+            'slam position',
+            past_img[y_from : y_to,:])
+        cv2.imshow(
+            'diff',
+            cv2.absdiff(
+                transformed_frames[-1].frame[-300:-200, 20:-20],
+                past_img[y_from : y_to,:]))
+
+        past_img, shift_x, shift_y = stich_two_images(
             past_img,
-            transformed_frames[-3].frame,
-            transformed_frames[-1].frame)
+            transformed_frames[-9].frame[300:,:],
+            transformed_frames[-1].frame[300:,:])
+        coord_y -= shift_y
+        print('y is', coord_y)
 
         cv2.imshow('slam', past_img)
 
