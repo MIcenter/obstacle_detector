07659b75aeddc38ffdc6c7200693658d5ec7cee5
sid1057 <ivanov.dale@gmail.com>
2017-10-27 22:00:48 +0300

    Test version of obstacle detect in half of original frame

--- depth_test.py
+++ depth_test.py
@@ -10,6 +10,7 @@ from obstacle_detector.perspective import inv_persp_new
 from obstacle_detector.perspective import regress_perspecive
 
 from obstacle_detector.depth_mapper import calculate_depth_map
+from obstacle_detector.tm.image_shift_calculator import find_shift_value
 
 def video_test(input_video_path=None, output_video_path=None):
     cx = 595
@@ -47,18 +48,62 @@ def video_test(input_video_path=None, output_video_path=None):
     left = cv2.imread('aloeL.jpg')
     right = cv2.imread('aloeR.jpg')
     while(ret):
+        original_frames.popleft()
         ret, frame = cap.read()
+        original_frames.append(frame)
 
         img, pts1 = inv_persp_new(
             frame, (cx, cy), (roi_width, roi_length), spline_dist, 200)
         old_images.popleft()
         old_images.append(img)
 
-        dm = calculate_depth_map(left, right)
-        dm = cv2.equalizeHist(dm)
-        cv2.imshow('dm', dm)
+        left = original_frames[-5][:, width // 2:]
+        right = original_frames[-1][:, width // 2:]
 
-        dm = cv2.cvtColor(dm, cv2.COLOR_GRAY2BGR)
+        left = cv2.pyrDown(left)
+        left = cv2.blur(left, (3, 3))
+        right = cv2.pyrDown(right)
+        right = cv2.blur(right, (3, 3))
+
+        depth = calculate_depth_map(left, right)
+        cv2.imshow('left', left)
+        cv2.imshow('right', right)
+        cv2.imshow('depth', depth)
+        depth = cv2.cvtColor(depth, cv2.COLOR_GRAY2BGR)
+        res = cv2.addWeighted(left, 0.5, depth, 0.5, 0)
+        cv2.imshow('res', res)
+
+#        left = old_images[-1][300:,:]
+#        right = old_images[-9][300:,:]
+#
+#        shift_value = find_shift_value(left, right, (30, 100, 60, 300))
+#        right = np.roll(right, shift_value[1], axis=0)#shift_value[0])
+#        right = np.roll(right, shift_value[0], axis=1)#shift_value[0])
+#        left = left[100:-100,:]
+#        right = right[100:-100,:]
+#
+#        print(shift_value)
+#
+#        left = np.rot90(left, 3)
+#        right = np.rot90(right, 3)
+#
+#        cv2.imshow('left', left)
+#        cv2.imshow('right', right)
+#
+#        shifted_map = cv2.equalizeHist(
+#            calculate_depth_map(
+#                left, right))
+#        cv2.imshow(
+#            'shifted map', shifted_map)
+#        diff = cv2.absdiff(left, right)
+#        cv2.imshow('diff', diff)
+
+#        dm = calculate_depth_map(left, right)
+#        cv2.imshow('dm', dm)
+#        dm = cv2.equalizeHist(dm)
+#        cv2.imshow('eq dm', dm)
+
+#        dm = cv2.cvtColor(dm, cv2.COLOR_GRAY2BGR)
 
         k = cv2.waitKey(1) & 0xff
         if k == 27:
--- obstacle_detector/depth_mapper.py
+++ obstacle_detector/depth_mapper.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2
 
 def calculate_depth_map(L, R):
-    imgL = cv2.pyrDown(L)  # downscale images for faster processing
-    imgR = cv2.pyrDown(R)
+    imgL = L
+    imgR = R
 
     # disparity range is tuned for 'aloe' image pair
     window_size = 3
@@ -11,13 +11,13 @@ def calculate_depth_map(L, R):
     num_disp = 112-min_disp
     stereo = cv2.StereoSGBM_create(minDisparity = min_disp,
         numDisparities = num_disp,
-        blockSize = 16,
+        blockSize = 2,
         P1 = 8*3*window_size**2,
         P2 = 32*3*window_size**2,
-        disp12MaxDiff = 1,
-        uniquenessRatio = 10,
+        disp12MaxDiff = 15,
+        uniquenessRatio = 15,
         speckleWindowSize = 100,
-        speckleRange = 32
+        speckleRange = 128
     )
 
     disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
@@ -27,10 +27,10 @@ def calculate_depth_map(L, R):
 
     max_value = max(res.ravel())
     min_value = min(res.ravel())
-    # res += min_value
-    # res /= max_value
-    # res *= 255
-    res = cv2.normalize(res, 0, 255, cv2.NORM_L1)
+    res -= min_value
+    res /= max_value
+    res *= 255
+#    res = cv2.normalize(res, 0, 255, cv2.NORM_L1)
     res = res.astype(np.uint8)
 
     return res
